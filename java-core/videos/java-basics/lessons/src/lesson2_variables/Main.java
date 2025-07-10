23b903a485a0b7c95d02a3a802a2c1a48641bd60
VadimSJJS <vadimvaluev72@gmail.com>
2025-07-10 21:40:40 +0300

    add lesson 2

---
+++ java-core/videos/java-basics/lessons/src/lesson2_variables/Main.java
@@ -0,0 +1,18 @@
+package lesson2_variables;
+
+public class Main {
+    public static void main(String[] args) {
+        int myInt = 557;
+        short myShort = 3266;
+        long myLong = 1234567890;
+
+        double myDouble = 235.35; // 64 бит
+        float myFloat = 2362.4f; // 32 бит
+
+        char c = 'a';
+        boolean b = true; // true or false
+
+        byte myByte = 100; // -128-127
+        System.out.println(myInt);
+    }
+}
