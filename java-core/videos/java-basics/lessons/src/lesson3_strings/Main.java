0c88095a0c810c03d9666dfb1d1bccece3f45256
VadimSJJS <vadimvaluev72@gmail.com>
2025-07-11 22:59:15 +0300

    videos java-basics

---
+++ java-core/videos/java-basics/lessons/src/lesson3_strings/Main.java
@@ -0,0 +1,13 @@
+package lesson3_strings;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 5; // примитвный тип данных
+        String s = "Hello"; // ссылочный тип данных
+        String space = " ";
+        String name = "Bob";
+        System.out.println(s + space + name);
+        System.out.println("Hello" + " " + "John");
+        System.out.println("My number is " + x);
+    }
+}
