7e8030b1b417ba3f16b2f31d8c345e80193cd566
VadimSJJS <vadimvaluev72@gmail.com>
2025-07-13 19:25:21 +0300

    Codintbat: tasks warmup-2

---
+++ java-core/exercises/codinngBat/src/warmup_2/stringTimes/Main.java
@@ -0,0 +1,29 @@
+package warmup_2.stringTimes;
+
+/*
+Given a string and a non-negative int n, return a larger
+string that is n copies of the original string.
+
+stringTimes("Hi", 2) → "HiHi"
+stringTimes("Hi", 3) → "HiHiHi"
+stringTimes("Hi", 1) → "Hi"
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(stringTimes("Hi", 2));
+        System.out.println(stringTimes("Hi", 3));
+        System.out.println(stringTimes("Hi", 1));
+    }
+
+    public static String stringTimes(String str, int n) {
+        String result = "";
+
+        for (int i = 0; i < n; i++) {
+            result += str;
+        }
+
+        return result;
+    }
+
+}
