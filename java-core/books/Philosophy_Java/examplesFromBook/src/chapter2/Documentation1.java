8971cfe1b65667d1ca1d20b48a6ecadc6d542574
VadimSJJS <vadimvaluev72@gmail.com>
2025-07-14 22:39:17 +0300

    Book Phylosophy Java: examples chapter 2

---
+++ java-core/books/Philosophy_Java/examplesFromBook/src/chapter2/Documentation1.java
@@ -0,0 +1,11 @@
+package chapter2;
+
+//: object/Documentation1.java
+
+/** Комментарий к классу */
+public class Documentation1 {
+    /** Комменатрий к переменной */
+    public int i ;
+    /** Комментарий к методу*/
+    public void f() {};
+} ///:~
---
+++ java-core/books/Philosophy_Java/examplesFromBook/src/chapter2/HelloDate.java
@@ -0,0 +1,10 @@
+package chapter2;
+
+import java.util.Date;
+
+public class HelloDate {
+    public static void main(String[] args) {
+        System.out.println("Привет, сегодня: ");
+        System.out.println(new Date());
+    }
+}
