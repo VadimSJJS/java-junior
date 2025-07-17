3d19e345fef020c917b637d0a8b457bb42766b6a
VadimSJJS <vadimvaluev72@gmail.com>
2025-07-17 18:14:00 +0300

    CodingBat: tasks String-1

---
+++ java-core/exercises/codinngBat/src/String_1/extraEnd.java
@@ -0,0 +1,24 @@
+package String_1;
+
+/*
+Given a string, return a new string made of 3 copies
+of the last 2 chars of the original string. The string
+length will be at least 2.
+
+extraEnd("Hello") → "lololo"
+extraEnd("ab") → "ababab"
+extraEnd("Hi") → "HiHiHi"
+ */
+
+public class extraEnd {
+    public static void main(String[] args) {
+        System.out.println(extraEnd("Hello"));
+        System.out.println(extraEnd("ab"));
+        System.out.println(extraEnd("HiHiHi"));
+    }
+
+    public static String extraEnd(String str) {
+        String newStr = str.substring(str.length() - 2);
+        return newStr + newStr + newStr;
+    }
+}
