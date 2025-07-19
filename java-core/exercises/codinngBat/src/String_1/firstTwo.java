40689f4b82da2e14e86e84b8575cf3b80da257a0
VadimSJJS <vadimvaluev72@gmail.com>
2025-07-19 23:12:31 +0300

    codingBat: String-1 tasks

---
+++ java-core/exercises/codinngBat/src/String_1/firstTwo.java
@@ -0,0 +1,30 @@
+package String_1;
+
+/*
+Given a string, return the string made of its first
+two chars, so the String "Hello" yields "He". If the
+string is shorter than length 2, return whatever there
+is, so "X" yields "X", and the empty string "" yields
+the empty string "". Note that str.length() returns
+the length of a string.
+
+firstTwo("Hello") → "He"
+firstTwo("abcdefg") → "ab"
+firstTwo("ab") → "ab"
+ */
+
+public class firstTwo {
+    public static void main(String[] args) {
+        System.out.println(firstTwo("Hello"));
+        System.out.println(firstTwo("abcdefg"));
+        System.out.println(firstTwo("ab"));
+    }
+
+    static public String firstTwo(String str) {
+        if (str.length() < 2) {
+            return str;
+        }
+
+        return str.substring(0, 2);
+    }
+}
