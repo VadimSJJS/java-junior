38920ac3f978a9bc574d85d786069229e7b10650
VadimSJJS <vadimvaluev72@gmail.com>
2025-07-20 19:57:38 +0300

    java-basics: video

---
+++ java-core/videos/java-basics/lessons/src/lesson29_modificators/Test.java
@@ -0,0 +1,10 @@
+package lesson29_modificators;
+
+public class Test {
+    protected int id;
+
+    public static void main(String[] args) {
+        // public, private, default(доступно в пределах пакета), protected(доступны в пределах одного пакета
+        // доступны всем подклассам, даже если эти подклассы вне пакета
+    }
+}
