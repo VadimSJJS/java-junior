2e3482e0ebe2a06a02573200a01f53ae12b61139
VadimSJJS <vadimvaluev72@gmail.com>
2025-07-20 20:15:00 +0300

    java-basics: video

---
+++ java-core/videos/java-basics/lessons/src/lesson32_classesWrappers/Main.java
@@ -0,0 +1,15 @@
+package lesson32_classesWrappers;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 1;
+
+        Integer x2 = 123;
+        int y = x2;
+        Integer.parseInt("123");
+
+        boolean s = Boolean.TRUE;
+        // Double, Float, Long, Integer, Short,
+        // Byte, Character and Boolean
+    }
+}
