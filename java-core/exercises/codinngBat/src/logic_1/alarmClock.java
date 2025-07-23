2c0d97d3bcd06eaa4a23a59d61eddbae38e7ea8f
VadimSJJS <vadimvaluev72@gmail.com>
2025-07-23 22:28:47 +0300

    CodingBat: Logic-1 tasks

---
+++ java-core/exercises/codinngBat/src/logic_1/alarmClock.java
@@ -0,0 +1,31 @@
+package logic_1;
+
+/*
+Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat,
+and a boolean indicating if we are on vacation, return a
+string of the form "7:00" indicating when the alarm clock
+should ring. Weekdays, the alarm should be "7:00" and on
+the weekend it should be "10:00". Unless we are on vacation
+-- then on weekdays it should be "10:00" and weekends it
+should be "off".
+
+alarmClock(1, false) → "7:00"
+alarmClock(5, false) → "7:00"
+alarmClock(0, false) → "10:00"
+ */
+
+public class alarmClock {
+    public static void main(String[] args) {
+
+    }
+
+    public static String alarmClock(int day, boolean vacation) {
+        if (vacation) {
+            // В отпуске: будни - "10:00", выходные - "off"
+            return (day >= 1 && day <= 5) ? "10:00" : "off";
+        } else {
+            // Не в отпуске: будни - "7:00", выходные - "10:00"
+            return (day >= 1 && day <= 5) ? "7:00" : "10:00";
+        }
+    }
+}
---
+++ java-core/exercises/codinngBat/src/logic_1/sortaSum.java
@@ -0,0 +1,25 @@
+package logic_1;
+
+/*
+Given 2 ints, a and b, return their sum. However, sums in
+the range 10..19 inclusive, are forbidden, so in that case
+just return 20.
+
+sortaSum(3, 4) → 7
+sortaSum(9, 4) → 20
+sortaSum(10, 11) → 21
+ */
+
+public class sortaSum {
+    public static void main(String[] args) {
+        System.out.println(sortaSum(3, 4));
+        System.out.println(sortaSum(9, 4));
+        System.out.println(sortaSum(10, 11));
+    }
+
+    public static int sortaSum(int a, int b) {
+        int sum = a + b;
+
+        return sum >= 10 && sum <= 19 ? 20 : sum;
+    }
+}
