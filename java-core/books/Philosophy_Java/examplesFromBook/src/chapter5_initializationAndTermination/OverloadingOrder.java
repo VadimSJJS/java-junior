b2adf080909528e3ce5e027251f4e927472ab4f2
VadimSJJS <vadimvaluev72@gmail.com>
2025-07-23 23:05:11 +0300

    Book philosophy Java: chapter 5

---
+++ java-core/books/Philosophy_Java/examplesFromBook/src/chapter5_initializationAndTermination/OverloadingOrder.java
@@ -0,0 +1,16 @@
+package chapter5_initializationAndTermination;
+
+public class OverloadingOrder {
+    static void f(String s, int i) {
+        System.out.println("String: " + s + ", int: " + i);
+    }
+
+    static void f(int i, String s) {
+        System.out.println("int: " + i + ", String: " + s);
+    }
+
+    public static void main(String[] args) {
+        f("Сначала строка", 11);
+        f(99, "Сначала число");
+    }
+}
